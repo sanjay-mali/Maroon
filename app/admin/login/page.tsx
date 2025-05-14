15a1a9ca1d25537b3bf9793d235a913539007b51
Sanjay Mali <sanjaymali94284@gmail.com>
2025-05-14 13:38:38 +0530

    refactor: comment out AdminLoginForm and related imports for future use ( for now ) THIS is TODO

--- app/admin/login/page.tsx
+++ app/admin/login/page.tsx
@@ -1,11 +1,11 @@
-import {AdminLoginForm} from '@/components/admin-login-form';
+// import {AdminLoginForm} from '@/components/admin-login-form';
 
 export default function LoginPage() {  
 
   return (
     <div className='w-full h-screen flex items-center justify-center'>
       <div className="bg-white p-8 rounded-lg shadow-md w-96">        
-        <AdminLoginForm />
+        {/* <AdminLoginForm /> */}
       </div>      
     </div>
   )
