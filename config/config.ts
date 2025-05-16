b3d874f75580fdb234b7cddb0cc19f2b8a13777f
Sanjay Mali <sanjaymali94284@gmail.com>
2025-05-16 14:39:49 +0530

    fix: correct typo in environment variable names in config

--- config/config.ts
+++ config/config.ts
@@ -1,9 +1,9 @@
 export const config = {
-  appwriteUrl: String(process.env.NEXT_PUNLIC_APPWRITE_URL),
-  appwriteProjectId: String(process.env.NEXT_PUNLIC_PROJECT_ID),
-  appwriteCollectionId: process.env.NEXT_PUNLIC_COLLECTION_ID,
-  appwriteBucketId: process.env.NEXT_PUNLIC_BUCKET_ID,
-  appwriteDatabaseId: process.env.NEXT_PUNLIC_DATABASE_ID,
+  appwriteUrl: String(process.env.NEXT_PUBLIC_APPWRITE_URL),
+  appwriteProjectId: String(process.env.NEXT_PUBLIC_PROJECT_ID),
+  appwriteCollectionId: process.env.NEXT_PUBLIC_COLLECTION_ID,
+  appwriteBucketId: process.env.NEXT_PUBLIC_BUCKET_ID,
+  appwriteDatabaseId: process.env.NEXT_PUBLIC_DATABASE_ID,
   announcementsCollectionId: "68233e9700242bd9a521",
   bannersCollectionId: "6819d90000299c92d966",
 };
